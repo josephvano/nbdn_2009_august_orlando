9a5de9e4b626e11a54355295092abd206a4afeb7
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-08-20 20:50:11 -0400

    wiring

--- store/product/nothinbutdotnetstore/win/core/ExpandNodeCommand.cs
+++ store/product/nothinbutdotnetstore/win/core/ExpandNodeCommand.cs
@@ -4,7 +4,7 @@ namespace nothinbutdotnetstore.win.core
 {
     public class ExpandNodeCommand : Command
     {
-        readonly TreeNode current_node;
+        TreeNode current_node;
         Command populate_command;
 
         public ExpandNodeCommand(TreeNode current_node, Command populate_command)
@@ -12,11 +12,9 @@ namespace nothinbutdotnetstore.win.core
             this.current_node = current_node;
             this.populate_command = populate_command;
         }
-
         public void run()
         {
             current_node.Nodes.Clear();
-
             populate_command.run();
         }
     }

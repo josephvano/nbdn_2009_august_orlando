fbb0a7b96ce7c06c5f15717441e3af1492778e2e
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2009-08-22 00:05:15 -0400

    added

---
+++ store/product/nothinbutdotnetstore/documenting/CommandAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace nothinbutdotnetstore.documenting
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CommandAttribute : Attribute
+    {
+        
+    }
+}
--- store/product/nothinbutdotnetstore/tasks/startup/InitializeTheContainer.cs
+++ store/product/nothinbutdotnetstore/tasks/startup/InitializeTheContainer.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using nothinbutdotnetstore.documenting;
 using nothinbutdotnetstore.infrastructure.containers.basic;
 
 namespace nothinbutdotnetstore.tasks.startup
 {
+    [Command]
     public class InitializeTheContainer : StartupCommand
     {
         public InitializeTheContainer(IDictionary<Type, Resolver> resolvers) {}

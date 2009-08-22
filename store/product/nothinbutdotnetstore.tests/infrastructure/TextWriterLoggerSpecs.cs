16ba53d86054e31511ed58e07de96857941dbd5c
unknown <jvano@.tib.ad.ea.com>
2009-08-22 00:59:11 -0400

    logging

--- store/product/nothinbutdotnetstore.tests/infrastructure/TextWriterLoggerSpecs.cs
+++ store/product/nothinbutdotnetstore.tests/infrastructure/TextWriterLoggerSpecs.cs
@@ -1,42 +1,40 @@
- using System.IO;
- using System.Text;
- using developwithpassion.bdd.contexts;
- using developwithpassion.bdd.mbunit.standard.observations;
- using developwithpassion.bdddoc.core;
- using developwithpassion.commons.core.infrastructure.logging;
- using nothinbutdotnetstore.infrastructure.logging.basic;
- using developwithpassion.bdd.mbunit;
+using System.IO;
+using System.Text;
+using developwithpassion.bdd.contexts;
+using developwithpassion.bdd.mbunit;
+using developwithpassion.bdd.mbunit.standard.observations;
+using developwithpassion.bdddoc.core;
+using developwithpassion.commons.core.infrastructure.logging;
+using nothinbutdotnetstore.infrastructure.logging.basic;
 
 namespace nothinbutdotnetstore.tests.infrastructure
- {   
-     public class TextWriterLoggerSpecs
-     {
-         public abstract class concern : observations_for_a_sut_with_a_contract<Logger,
-                                             TextWriterLogger>
-         {
-        
-         }
+{
+    public class TextWriterLoggerSpecs
+    {
+        public abstract class concern : observations_for_a_sut_with_a_contract<Logger,
+                                            TextWriterLogger> {}
 
-         [Concern(typeof(TextWriterLogger))]
-         public class when_logging_messages : concern
-         {
-             context c = () =>
-             {
-                provide_a_basic_sut_constructor_argument<TextWriter>(new StringWriter(builder)); 
-             };
+        [Concern(typeof (TextWriterLogger))]
+        public class when_logging_messages : concern
+        {
+            context c = () =>
+            {
+                builder = new StringBuilder();
+                provide_a_basic_sut_constructor_argument<TextWriter>(new StringWriter(builder));
+            };
 
-             because b = () =>
-             {
-                 sut.informational("hello world");
-             };
+            because b = () =>
+            {
+                sut.informational("hello world");
+            };
 
-        
-             it should_log_the_message_to_its_text_writer = () =>
-             {
+
+            it should_log_the_message_to_its_text_writer = () =>
+            {
                 builder.ToString().should_be_equal_ignoring_case("hello world\r\n");
-             };
+            };
 
-             static StringBuilder builder;
-         }
-     }
- }
+            static StringBuilder builder;
+        }
+    }
+}
--- store/product/nothinbutdotnetstore/infrastructure/logging/basic/TextWriterLogger.cs
+++ store/product/nothinbutdotnetstore/infrastructure/logging/basic/TextWriterLogger.cs
@@ -1,13 +1,21 @@
 using System;
+using System.IO;
 using developwithpassion.commons.core.infrastructure.logging;
 
 namespace nothinbutdotnetstore.infrastructure.logging.basic
 {
     public class TextWriterLogger : Logger
     {
+        TextWriter text_writer;
+
+        public TextWriterLogger(TextWriter text_writer)
+        {
+            this.text_writer = text_writer;
+        }
+
         public void informational(string message)
         {
-            throw new NotImplementedException();
+            text_writer.WriteLine(message);
         }
 
         public void error(Exception exception)
